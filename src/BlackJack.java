7576af4e3d790ff777a7d137677767c86d1b4ee4
Zsolt Nagy <nagyza@gmail.com>
2017-04-18 12:01:59 +0200

    Create BlackJack class for manual test

---
+++ src/BlackJack.java
@@ -0,0 +1,10 @@
+
+public class BlackJack {
+  public static void main(String[] args) {
+    Deck deck = new Deck(12);
+    System.out.println(deck);
+    Card drawn = deck.draw();
+    System.out.println(drawn);
+    System.out.println(deck);
+  }
+}
